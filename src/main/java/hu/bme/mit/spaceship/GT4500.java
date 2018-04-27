4c9a78f736eb6f125e12b179cbe381509692316b
meres <kiskovi97@gmail.com>
2018-04-27 03:57:05 -0700

    enumproblem

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -38,7 +38,7 @@ public class GT4500 implements SpaceShip {
 
     boolean firingSuccess = false;
 
-    if (firingMode == SINGLE) {
+    if (firingMode == FiringMode.SINGLE) {
         if (wasPrimaryFiredLast) {
           // try to fire the secondary first
           if (! secondaryTorpedoStore.isEmpty()) {
@@ -106,7 +106,7 @@ public class GT4500 implements SpaceShip {
             // if both of the stores are empty, nothing can be done, return failure
           }
        
-    }
+    	}
 
     return firingSuccess;
   }
